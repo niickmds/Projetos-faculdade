100a159fd454210a4652a46ea06efb118d471af4
Nick <128084163+niickmds@users.noreply.github.com>
2023-10-27 00:44:26 -0300

    Add files via upload

---
+++ Projeto-domino/Dom_KLMN_Model.h
@@ -0,0 +1,11 @@
+//DOM-KLMN - Projeto Domino - Etapa 6
+//20/09/2023
+
+#define MAX 28
+int numeroJogadas;
+int J; //jogador da vez
+int indicePeca; //indice das pecas (a-h)
+bool fimJogo;
+int opc; //opcao de escolha do menu
+bool jogoMaquina; //se o jogo for contra a maquina
+int qtdPecasDeposito;
---
+++ Projeto-domino/Dom_KLMN_View.h
@@ -0,0 +1,12 @@
+//DOM-KLMN - Projeto Domino - Etapa 6
+//20/09/2023
+
+void fMenuInicio ();
+void fPrintPrimeiroJogador();
+void fMensagem ();
+void fMesa ();
+void fPrintPecas (int jogador);
+void fPrintVencedor(int jogador);
+char fEscolhaChar();
+void fPrintResultadoMaquina(int compras, bool jogadaFoiPassada);
+void fPrintRegras();
